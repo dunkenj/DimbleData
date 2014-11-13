edec9974a37074773fd2c8e6613206d285237bf6
dunkenj <ken@ngm21.plus.com>
2014-11-13 21:44:54 +0000

    New versions

--- HESELTINE.py
+++ HESELTINE.py
@@ -3,7 +3,7 @@ from collections import Counter
 import urllib2, numpy
 import matplotlib.pyplot as plt
 
-response = urllib2.urlopen('http://en.wikipedia.org/wiki/Nigel_Farage')
+response = urllib2.urlopen('http://en.wikipedia.org/wiki/Chris_Huhne')
 html = response.read()
 
 soup = b(html)
@@ -17,6 +17,23 @@ for tr in trs:
         if k.text == 'Political party':
             t = tr.findAll('td')
             ass = t[0].findAll('a')
-            print ass[-1].text
-            break
+            if len(ass) >= 1:
+                print ass[-1].text
+                break
             
+text = [p.getText() for p in soup.findAll('p')]
+ttext = "".join(text).lower()
+
+C = Counter(ttext.split())
+print C['he'], C['she']
+
+if C['he'] > C['she']:
+    tonk = 'Male'
+
+elif C['she'] > C['he']:
+    tonk = 'Female'
+
+else:
+    tonk = 'Not Found'
+    
+print tonk
--- QTstats2.py
+++ QTstats2.py
@@ -3,7 +3,7 @@ from collections import Counter
 import urllib2, numpy
 import matplotlib.pyplot as plt
 import sqlite3 as lite
-
+import numpy
 # Creates or opens a file called mydb with a SQLite3 DB
 db = lite.connect('QTdb')
 
@@ -14,6 +14,8 @@ html = response.read()
 soup = b(html)
 
 people = []
+genders = []
+parties = []
 
 tables = soup.findAll('table','wikitable')[2:] #First two tables are other content
 year_headers = soup.findAll('h2')[2:-4] # Likewise with headers
@@ -36,15 +38,32 @@ def find_party_get_party_party_on(url):
             if k.text == 'Political party':
                 t = tr.findAll('td')
                 ass = t[0].findAll('a')
-                drunk = ass[-1].text
-                break
-    return drunk
+                if len(ass) >= 1:
+                    drunk = ass[-1].text
+                    break
+                
+    text = [p.getText() for p in poop.findAll('p')[:5]]
+    conc_text = "".join(text).lower()
+
+    C = Counter(conc_text.split())
+    #print C['he'], C['she']
+
+    if C['he'] > C['she']:
+        tonk = 'Male'
+
+    elif C['she'] > C['he']:
+        tonk = 'Female'
+
+    else:
+        tonk = 'Not Found'
+    
+    return drunk, tonk
 
 for year in year_headers:
     spans = year.findAll('span')
     years.append(int(spans[0].text))
 
-for i, table in enumerate(tables[-1:]):
+for i, table in enumerate(tables[-4:]):
     print i
     for row in table.findAll('tr'):
         cols = row.findAll('td')
@@ -54,23 +73,41 @@ for i, table in enumerate(tables[-1:]):
             for link in links:
                 purl = 'http://en.wikipedia.org'+link.get('href')
                 if '#' not in purl:
-                    #print link.text,
-                    #print purl,
-                    party = find_party_get_party_party_on(purl)
-                    #print party
+                    print link.text,
+                    print purl,
+                    party, gender = find_party_get_party_party_on(purl)
+                    print party, gender
+                    genders.append(gender)
+                    parties.append(party)
                     names.append(link.text)
+                    people.append(link.text)
                     
             names2 = cols[2]            
             nstring = names2.getText().split(',')            
             for name in nstring:
                 if name not in names:
                     names.append(name)
-            print names
+                    people.append(name)
+            #print names
         else:
             continue
 
 
 counts = Counter(people)
+
+gendercount = Counter(genders)
+
+parties = numpy.array(parties)
+
+parties[parties == 'Scottish Conservative Party'] = 'Conservative'
+parties[parties == 'Conservative Party'] = 'Conservative'
+parties[parties == 'Labour Party'] = 'Labour'
+parties[parties == 'Labour Co-operative'] = 'Labour'
+parties[parties == 'Liberal Democrat'] = 'Liberal Democrats'
+parties[parties == '[1]'] = 'Unknown'
+
+partycount = numpy.array(Counter(parties).most_common(8)[1:])
+
 
 order = numpy.argsort(counts.values())
 names = numpy.array(counts.keys())[order][::-1]
@@ -113,6 +150,23 @@ Ax.set_ylim([amin-1,amax+1])
 Ax.set_title('Top '+str(N)+' QT guests')
 Fig.subplots_adjust(bottom=0.26,right=0.95,left=0.07)
 Fig.savefig('QTappearances.png',fmt='png')
+
+Fig2 = plt.figure(figsize=(8,6))
+Ax2 = Fig2.add_subplot(111)
+
+Ax2.pie(partycount[:,1],labels=partycount[:,0],autopct='%.1f%%',
+        pctdistance=0.8)
+Ax2.axis('equal')
+
+Fig3 = plt.figure(figsize=(8,6))
+Ax3 = Fig3.add_subplot(111)
+
+Ax3.pie(gendercount.values(),labels=gendercount.keys(),autopct='%.1f%%')
+Ax3.axis('equal')
+
 plt.show()
+
+
+
 
 db.close()
